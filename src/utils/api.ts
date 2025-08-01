05d3ff15925a8ce57407c357d7f22b0b3b4925e7
Syafiq <syafiqmusoffa@gmail.com>
2025-08-01 13:54:44 +0700

    fix error

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -2,7 +2,7 @@ import { useAuthStore } from "@/store/auth";
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "http://localhost:3000/api/v1",
+  baseURL: import.meta.env.VITE_API_URL,
 });
 
 // ✅ Interceptor untuk inject token otomatis
